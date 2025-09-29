3131d12f60b66d5269be9cfa9a8bff0edda98654
Amit Jain <ajain@offbeatsoftwaresolutions.in>
2025-09-29 13:52:29 +0530

    initial commit
    initial commit

---
+++ Apps/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld.Apps
+{
+    [App(icon: Icons.Box)]
+    public class HelloWorldPage : ViewBase
+    {
+        public override object? Build()
+        {
+            return "HelloWorld";
+        }
+    }
+}
